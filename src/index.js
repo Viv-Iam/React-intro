b5285436733b798e373cd4e1e16adad8083affeb
Vivi-Iam <vivian.opondoh@gmail.com>
2019-04-09 17:30:30 +0300

    Add unordered list to index.js and render it on index.html

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 
 
 ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById('root'));
+ReactDOM.render(<ul><li>Vivian</li><li>Awino</li><li>Opondoh</li></ul>, document.getElementById('body'));
 
