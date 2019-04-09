258c97bef0d400ae1b4e4331adca7070c67d8306
Vivi-Iam <vivian.opondoh@gmail.com>
2019-04-09 19:28:01 +0300

    Move the unordered list into a functional component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function MyList() {
+    return (
+    <ul>
+        <li>Vivian</li>
+        <li>Awino</li>
+        <li>Opondoh</li>
+    </ul>
+)
+}
 
 ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById('root'));
-ReactDOM.render(<ul><li>Vivian</li><li>Awino</li><li>Opondoh</li></ul>, document.getElementById('body'));
+ReactDOM.render(<MyList />, document.getElementById('body'));
 
