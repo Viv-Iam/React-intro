8c3cd01e082957a54d3d6f6ac81a2fdbd55e4dd3
Vivi-Iam <vivian.opondoh@gmail.com>
2019-04-10 13:15:44 +0300

    Create Components folder and add individual folders in own separate file

---
+++ src/Components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyInfo() {
+    return (
+        <div>
+        <h1>Vivian Opondoh</h1>
+        <p>I am an entrepreneur, I love love cooking, This is my year with my solid team</p>
+        <ol>
+            <li>Bali</li>
+            <li>Maldives</li>
+            <li>Dar es Salaam</li>
+        </ol>
+        </div>
+    )
+}
+
+export default MyInfo;
---
+++ src/Components/MyList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MyList() {
+    return (
+    <ul>
+        <li>Vivian</li>
+        <li>Awino</li>
+        <li>Opondoh</li>
+    </ul>
+)
+}
+
+export default MyList;
