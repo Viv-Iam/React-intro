08143a8a6a579e07b38485d5b33ae178356348fb
Vivi-Iam <vivian.opondoh@gmail.com>
2019-04-10 13:15:06 +0300

    Remove components and import new individual component files

--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './index.css'
-
-function MyList() {
-    return (
-    <ul>
-        <li>Vivian</li>
-        <li>Awino</li>
-        <li>Opondoh</li>
-    </ul>
-)
-}
-
-function MyInfo() {
-    return (
-        <div>
-        <h1>Vivian Opondoh</h1>
-        <p>I am an entrepreneur, I love love cooking, This is my year with my solid team</p>
-        <ol>
-            <li>Bali</li>
-            <li>Maldives</li>
-            <li>Dar es Salaam</li>
-        </ol>
-        </div>
-    )
-}
+import MyInfo from './Components/MyInfo'
+import MyList from './Components/MyList'
 
 ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById('root'));
 ReactDOM.render(<MyList />, document.getElementById('body'));
