3339471410cbb6d15d52d1ec15a1ad43da2e2171
Vivi-Iam <vivian.opondoh@gmail.com>
2019-04-10 12:31:56 +0300

    Add new functional component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
 
 function MyList() {
     return (
@@ -11,6 +12,21 @@ function MyList() {
 )
 }
 
+function MyInfo() {
+    return (
+        <div>
+        <h1>Vivian Opondoh</h1>
+        <p>I am an entrepreneur, I love love cooking, This is my year with my solid team</p>
+        <ol>
+            <li>Bali</li>
+            <li>Maldives</li>
+            <li>Dar es Salaam</li>
+        </ol>
+        </div>
+    )
+}
+
 ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById('root'));
 ReactDOM.render(<MyList />, document.getElementById('body'));
+ReactDOM.render(<MyInfo />, document.getElementById('body1'));
 
