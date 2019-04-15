467d16ec6d9fd296740c50df5154029e9fbc3447
Vivi-Iam <vivian.opondoh@gmail.com>
2019-04-15 16:36:11 +0300

    Add styling not display  que when no que and ans diff color

--- src/Components/Joke.js
+++ src/Components/Joke.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Joke = (props) => (
     <div>
-    <h2> Question: {props.text.question} </h2>
-    <h3> PunchLine: {props.text.punchline} </h3>
+    <h2 style = {{display: !props.text.question && "none"}}> Question: {props.text.question} </h2>
+    <h3 style = {{color: !props.text.question && "blue"}}> PunchLine: {props.text.punchline} </h3>
     <hr/>
     </div>
 ) 
