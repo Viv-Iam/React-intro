c1745cc9e86d9891d48e1f0ee9583a609f1bb488
Vivi-Iam <vivian.opondoh@gmail.com>
2019-04-15 16:03:51 +0300

    Comment out unused components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import MyInfo from './Components/MyInfo'
-import MyList from './Components/MyList'
+// import MyInfo from './Components/MyInfo'
+// import MyList from './Components/MyList'
 import App from './App'
 
 // ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'))
-ReactDOM.render(<MyList />, document.getElementById('body'));
-ReactDOM.render(<MyInfo />, document.getElementById('body1'));
+// ReactDOM.render(<MyList />, document.getElementById('body'));
+// ReactDOM.render(<MyInfo />, document.getElementById('body1'));
 
