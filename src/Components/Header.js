79ba37df335bbcd9ad75038b722ea3da22bd8b2f
Vivi-Iam <vivian.opondoh@gmail.com>
2019-04-15 16:01:25 +0300

    Add Joke component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,12 +5,12 @@ function Header() {
         <header>
             <div>
                 <h1>Vivian Opondoh</h1>
-                <p>I am an entrepreneur, I love love cooking, This is my year with my solid team</p>
+                {/* <p>I am an entrepreneur, I love love cooking, This is my year with my solid team</p>
                 <ol>
                     <li>Bali</li>
                     <li>Maldives</li>
                     <li>Dar es Salaam</li>
-                </ol>
+                </ol> */}
             </div>
         </header>
     )
